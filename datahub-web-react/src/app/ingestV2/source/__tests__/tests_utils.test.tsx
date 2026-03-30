ccae5c4f6c290f86090c46b5d8adacdb34c333fe
purnimagarg1 <purnima.garg@apptware.com>
2026-03-30 15:21:05 +0530

    fix tests

--- datahub-web-react/src/app/ingestV2/source/__tests__/tests_utils.test.tsx
+++ datahub-web-react/src/app/ingestV2/source/__tests__/tests_utils.test.tsx
@@ -570,7 +570,7 @@ describe('formatTimezone', () => {
         expect(['EST', 'EDT']).toContain(nycAbbr);
 
         const londonAbbr = formatTimezone('Europe/London');
-        expect(['GMT', 'BST']).toContain(londonAbbr);
+        expect(['GMT', 'BST', 'GMT+1']).toContain(londonAbbr);
 
         // Tokyo doesn't observe DST, so it's always GMT+9
         expect(formatTimezone('Asia/Tokyo')).toBe('GMT+9');
